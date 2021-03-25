4a40803b8a3e2c87867eea331a3e22acf35754a8
Morten Borup Petersen <morten_bp@live.dk>
2021-03-25 10:20:16 +0100

    Make Chisel test example more tweakable

--- test_regress/t_chisel/t_longcond/src/main/scala/t_longcond.scala
+++ test_regress/t_chisel/t_longcond/src/main/scala/t_longcond.scala
@@ -6,10 +6,14 @@ import chisel3.util.Cat
 class T_longcond(depth : Int) extends Module {
   val nFuncUnits = (math.pow(2, depth) - 1).toInt
   val io = IO(new Bundle {
-    val in = Input(UInt(32.W))
+    val in1 = Input(UInt(32.W))
+    val in2 = Input(UInt(32.W))
     val doComp = Input(UInt(nFuncUnits.W))
     val out = Output(UInt(32.W))
   })
+
+  val COND_COMPLEXITY = 10 // defines the computation size of the comparison value
+  val COMP_COMPLEXITY = 10 // defines the computation size of the computation value
 
   val regs = Reg(Vec(nFuncUnits, (UInt(32.W))))
   val adders = Wire(Vec(nFuncUnits, UInt(32.W)))
@@ -19,16 +23,31 @@ class T_longcond(depth : Int) extends Module {
   io.out := adders.slice((nFuncUnits + 1)/2, nFuncUnits).reduce(_*_)
   regs := adders.zipWithIndex.map({case (adder, i) => Mux(regEn, adder, regs(i))})
   regEn := addCond.reduce(_ & _)
-  addCond := (io.in > regs(0)) +: (1 until nFuncUnits).map{i => regs(i) > regs(i-1)}
+  addCond := (heavyBooleanComp(io.in1, io.in2, regs(0))) +: (1 until nFuncUnits).map{i => heavyBooleanComp(regs(i), io.in2, regs(i-1))}
 
   for(i <- 0 until nFuncUnits) {
     val preIdx = (i-1)/2 
-    val op1 = if (i == 0) io.in else adders(preIdx)
+    val op1 = if (i == 0) io.in1 else adders(preIdx)
     adders(i) := Mux(addCond(i), op1 + regs(i), regs(i) * 2.U)
   }
+
+  private def heavyBooleanComp(i: UInt, j: UInt, k: UInt) : Bool = {
+    def compStuff(b: UInt, c: UInt, d: UInt, round: Int) : UInt = {
+      round % 4 match {
+        case 0 => (b & c) | (~b & d)
+        case 1 => (b & d) | (c & ~d)
+        case 2 => (b ^ c ^ d)
+        case 3 => c ^ (b | ~d)
+      }
+    }
+
+    (0 until COND_COMPLEXITY).foldLeft(i){case (acc, i) => compStuff(acc, j, k, i)} === 42.U
+  }
+
+  // private def heavyCombComp()
 }
 
 
 object T_longcond extends App {
-  (new chisel3.stage.ChiselStage).emitVerilog(new T_longcond(3))
+  (new chisel3.stage.ChiselStage).emitVerilog(new T_longcond(2))
 }
